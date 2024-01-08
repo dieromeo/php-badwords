0dbdc54b04d404f5fc2b82063e314f5eec9ee235
dieromeo <diegoromeo99@icloud.com>
2024-01-08 16:01:00 +0100

    feat: form style

--- form/form.php
+++ form/form.php
@@ -3,15 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css  -->
+    <link rel="stylesheet" href="style.css">
     <title>Bad words form</title>
 </head>
 <body>
-    <form action="../response/index.php" method="get">
+    <header>
+        <h1>Bad word Form</h1>
+    </header>
+    <main>
+<!-- form  -->
+<form action="../response/index.php" method="get">
+        <!-- textarea  -->
+        <div>
         <label for="text">Insert text:</label>
         <textarea name="text" id="text" cols="30" rows="10"></textarea>
-        <label for="bad-word">Insert a bad word</label>
+        </div>
+        <!-- input text  -->
+        <div class="bad-word">
+        <label for="bad-word">Insert a bad word:</label>
         <input type="text" name="bad-word">
-        <button>Send</button>
+        </div>
+        <!-- button  -->
+        <button id="submit">Send</button>
     </form>
+    </main>
 </body>
 </html>
