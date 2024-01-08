ed847f58a5805bdaa62b9ddf4627d5be9a1a8cbf
dieromeo <diegoromeo99@icloud.com>
2024-01-08 16:18:22 +0100

    feat: response style

--- response/index.php
+++ response/index.php
@@ -8,16 +8,28 @@ $bad_word= $_GET["bad-word"]
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css  -->
+    <link rel="stylesheet" href="style.css">
     <title>Form result</title>
 </head>
 <body>
-    <p><?php echo $text ?> </p>
+    <header>
+    <h1>Form Result</h1>
+    </header>
+    <main>
+        <div class="container">
+        <div class="original-text">
+        <h2>Original text</h2>
+    <p><strong>Text:</strong> <?php echo $text ?> </p>
     <p>
-        <?php echo strlen($text) ?>
+    <strong>Length:</strong> : <?php echo strlen($text) ?>
     </p>
+    </div>
 
     <h2>Replaced Text</h2>
-    <h4> <?php echo str_replace($bad_word, '***', $text) ?></h4>
-    <h4> <?php echo strlen(str_replace($bad_word, '***', $text)) ?></h4>
+    <p><strong>Text:</strong>  <?php echo str_replace($bad_word, '***', $text) ?></p>
+    <p><strong>Legth:</strong>  <?php echo strlen(str_replace($bad_word, '***', $text)) ?></p>
+        </div>
+    </main>
 </body>
 </html>
